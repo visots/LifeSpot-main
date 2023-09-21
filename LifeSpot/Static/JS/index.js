aeb06e1a80f8fb29a6493fed67946eb90c0f855e
avisots <visotskiy.alexey@gmail.com>
2023-09-22 00:01:01 +0700

    map

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,9 +1,16 @@
 let age = prompt("Введите ваш возраст");
-
+let time = new Date().toLocaleString()
 if(age >= 18){
-    alert("Приветствуем на LifeSpot! " + new Date().toLocaleString());
+    alert("Приветствуем на LifeSpot! " + time);
 }
 else {
     alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
     window.location.href = "http://www.google.com";
+}
+let userData= new Map();
+userData.set('age',age);
+userData.set('userAgent',window.navigator.userAgent);
+userData.set('sessionStart',  time);
+for (let result of session){
+    console.log(result)
 }
