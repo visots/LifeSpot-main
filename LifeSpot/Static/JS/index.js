cf5d05bcc4cb6431a69dcda79cbf1f3a95394fd1
avisots <visotskiy.alexey@gmail.com>
2023-09-23 22:03:46 +0700

    Функции,замыкания и прочие радости

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,16 +1,36 @@
-let age = prompt("Введите ваш возраст");
-let time = new Date().toLocaleString()
-if(age >= 18){
-    alert("Приветствуем на LifeSpot! " + time);
+ function handleSessionStart() {
+    let age = prompt("Введите ваш возраст");
+    let time = new Date().toLocaleString()
+    if (age >= 18) {
+        alert("Приветствуем на LifeSpot! " + time);
+    } else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com";
+    }
+    let userData = new Map();
+    userData.set('age', age);
+    userData.set('userAgent', window.navigator.userAgent);
+    userData.set('sessionStart', time);
+    return userData;
 }
-else {
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-    window.location.href = "http://www.google.com";
+
+let sessionLog = function logSession(session) {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
 }
-let userData= new Map();
-userData.set('age',age);
-userData.set('userAgent',window.navigator.userAgent);
-userData.set('sessionStart',  time);
-for (let result of session){
-    console.log(result)
+function filterContent(inputParceFunc)
+{
+    //let value = document.getElementsByTagName('input')[0].value.toLowerCase();
+    //let value = filterValue.toLowerCase();
+    let videoElements = document.getElementsByClassName('video-container');
+    for(let i = 0; i<= videoElements.length; i++)
+    {
+        let description = videoElements[i].  getElementsByTagName('h3')[0].innerText.toLowerCase();
+        if(!description.includes(inputParceFunc()))
+            videoElements[i].style.display ='none';
+        else
+            videoElements[i].style.display ='inline-block';
+    } 
 }
