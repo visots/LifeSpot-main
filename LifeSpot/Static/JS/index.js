996a91072b1cff5c63734f327a2ad6313d3f70b3
avisots <visotskiy.alexey@gmail.com>
2023-09-24 15:08:07 +0700

    callback

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -20,17 +20,16 @@ let sessionLog = function logSession() {
         console.log(result)
     }
 }
-function filterContent(inputParceFunc)
-{
-    //let value = document.getElementsByTagName('input')[0].value.toLowerCase();
-    //let value = filterValue.toLowerCase();
-    let videoElements = document.getElementsByClassName('video-container');
-    for(let i = 0; i<= videoElements.length; i++)
-    {
-        let description = videoElements[i].  getElementsByTagName('h3')[0].innerText.toLowerCase();
-        if(!description.includes(inputParceFunc()))
-            videoElements[i].style.display ='none';
-        else
-            videoElements[i].style.display ='inline-block';
-    } 
+function filterContent(){
+    let elements = document.getElementsByClassName('video-container');
+
+    for (let i = 0; i <= elements.length; i++ ){
+        let videoText = elements[i].querySelector(".video-title").innerText;
+        if(!videoText.toLowerCase().includes(inputParseFunction().toLowerCase())){
+            elements[i].style.display = 'none';
+        } else {
+            elements[i].style.display = 'inline-block';
+        }
+    }
 }
+
